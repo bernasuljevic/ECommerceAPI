191dbdc671ac84e578f64268e53058833d8fe07b
Berna Suljevic <bernasuljevic8@gmail.com>
2026-06-22 15:36:35 +0300

    Added GetById and Count endpoints

--- ECommerceAPI/Controllers/ProductsController.cs
+++ ECommerceAPI/Controllers/ProductsController.cs
@@ -5,32 +5,39 @@ namespace ECommerceAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
     public class ProductsController : ControllerBase
     {
+            private static readonly List<Product> Products = new()
+        {
+        new Product { Id = 1, Name = "Laptop", Price = 35000 },
+        new Product { Id = 2, Name = "Mouse", Price = 500 },
+        new Product { Id = 3, Name = "Klavye", Price = 1200 }
+        };
+
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>
-            {
-                new Product
-                {
-                    Id = 1,
-                    Name = "Laptop",
-                    Price = 35000
-                },
-                new Product
-                {
-                    Id = 2,
-                    Name = "Mouse",
-                    Price = 500
-                },
-                new Product
-                {
-                    Id = 3,
-                    Name = "Klavye",
-                    Price = 1200
-                }
-            };
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+            return Products.FirstOrDefault(p => p.Id == id);
+        }
+        [HttpGet("count")]
+        public int GetCount()
+        {
+            return Products.Count;
+        }
+
+        [HttpPost]
+        public Product AddProduct(Product product)
+        {
+            Products.Add(product);
+
+            return product;
         }
     }
 }
