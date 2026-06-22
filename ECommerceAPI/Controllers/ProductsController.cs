0157782035fe7f2c9da5325b2139b2b43da77902
Berna Suljevic <bernasuljevic8@gmail.com>
2026-06-22 14:19:52 +0300

    Proje dosyası ekle.

---
+++ ECommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>
+    {
+        "Laptop",
+        "Mouse",
+        "Klavye"
+    };
+        }
+    }
+}
